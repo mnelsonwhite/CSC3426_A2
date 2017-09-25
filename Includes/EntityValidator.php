4020f41d751b077b0463fd52426fab291f8cb6dc
Matthew Nelson-White <matthew.nelson-white@dcsgroup.com.au>
2017-09-25 11:31:51 +1000

    Add validation classes and tests

--- Includes/EntityValidator.php
+++ Includes/EntityValidator.php
@@ -9,10 +9,30 @@ class EntityValidator
         $this->dbSchema = $dbSchema;
     }
 
-    public function IsValid($model, $validationModel) : bool
+    public function IsValid($model) : bool
     {
 
         return false;
+    }
+
+    public function IsFieldDataTypeCorrect($model, $fieldName, $type)
+    {
+        switch($type)
+        {
+            case "TEXT":
+                return !isset($model->$fieldName) || is_string($model->$fieldName);
+            break;
+            case "INTEGER":
+                return !isset($model->$fieldName) || $this->is_int($model->$fieldName);
+            break;
+            default:
+                throw new Exception("Unsupported data type");
+        }
+    }
+
+    // true on string or numeric integer value
+    private function is_int($input){
+        return(ctype_digit(strval($input)));
     }
 }
 
